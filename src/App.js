ee4beefcb335ce56a43e88b9597238ebc21caad4
danshika42 <83402199+danshika42@users.noreply.github.com>
2022-05-26 13:59:52 +0530

    --initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="w-full bg-red-500 h-10">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
